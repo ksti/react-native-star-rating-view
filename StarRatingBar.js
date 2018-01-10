303dceb56cadb7207be8704c4172b2f268a4320d
gjs <1353990812@qq.com>
2018-01-10 19:10:26 +0800

    Fall back to View.propTypes if ViewPropTypes is not available

--- StarRatingBar.js
+++ StarRatingBar.js
@@ -42,38 +42,43 @@ import {
     View,
     Text,
     StyleSheet,
-    TouchableHighlight
+    TouchableHighlight,
+    ViewPropTypes
 } from 'react-native'
+
+import PropTypes from 'prop-types'
 
 import StarRatingView from './StarRatingView'
 
+const RNViewPropTypes = ViewPropTypes || View.propTypes;
+const RNPropTypes = PropTypes || React.PropTypes;
 const propTypes = {
-    style: View.propTypes.style,
-    starStyle: View.propTypes.style, // 自定义星星样式
-    readOnly: React.PropTypes.bool, // 是否只读
-    continuous: React.PropTypes.bool, // 是否允许滑动打分
-    maximumValue: React.PropTypes.number, // 最大值
-    minimumValue: React.PropTypes.number, // 最小值
-    value: React.PropTypes.number, // 具体数值
-    valueToFix: React.PropTypes.number, // 保留几位小数
-    spacing: React.PropTypes.number, // 分数
-    allowsHalfStars: React.PropTypes.bool, // 是否允许半颗星
-    accurateHalfStars: React.PropTypes.bool, // 是否允许精确值
+    style: RNViewPropTypes.style,
+    starStyle: RNViewPropTypes.style, // 自定义星星样式
+    readOnly: RNPropTypes.bool, // 是否只读
+    continuous: RNPropTypes.bool, // 是否允许滑动打分
+    maximumValue: RNPropTypes.number, // 最大值
+    minimumValue: RNPropTypes.number, // 最小值
+    value: RNPropTypes.number, // 具体数值
+    valueToFix: RNPropTypes.number, // 保留几位小数
+    spacing: RNPropTypes.number, // 分数
+    allowsHalfStars: RNPropTypes.bool, // 是否允许半颗星
+    accurateHalfStars: RNPropTypes.bool, // 是否允许精确值
     /* todo: 绘制星星图片
-     starBorderColor: React.PropTypes.string, // 星星边线颜色
-     starBorderWidth: React.PropTypes.number, // 星星边线宽度
+     starBorderColor: RNPropTypes.string, // 星星边线颜色
+     starBorderWidth: RNPropTypes.number, // 星星边线宽度
      */
-    emptyStarColor: React.PropTypes.string, // 空星填充色
-    tintColor: React.PropTypes.string, // 着色(填充色)
-    emptyStarImage: React.PropTypes.element, // 空星图片
-    halfStarImage: React.PropTypes.element, // 半星图片
-    filledStarImage: React.PropTypes.element, // 实星图片
-    onStarValueChanged: React.PropTypes.func, // 数值改变时的回调函数
+    emptyStarColor: RNPropTypes.string, // 空星填充色
+    tintColor: RNPropTypes.string, // 着色(填充色)
+    emptyStarImage: RNPropTypes.element, // 空星图片
+    halfStarImage: RNPropTypes.element, // 半星图片
+    filledStarImage: RNPropTypes.element, // 实星图片
+    onStarValueChanged: RNPropTypes.func, // 数值改变时的回调函数
 
     // 额外的属性
-    scoreTextStyle: React.PropTypes.object, // 自定义分数文本样式
-    scoreText: React.PropTypes.string, // 分数文本
-    dontShowScore: React.PropTypes.bool, // 不显示分数
+    scoreTextStyle: RNPropTypes.object, // 自定义分数文本样式
+    scoreText: RNPropTypes.string, // 分数文本
+    dontShowScore: RNPropTypes.bool, // 不显示分数
 };
 
 const defaultProps = {
